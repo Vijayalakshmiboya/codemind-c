5a57f9cc543c5a6d7ef0bd380d56d65170515f1c
Vijayalakshmiboya <98221516+Vijayalakshmiboya@users.noreply.github.com>
2022-06-29 22:47:46 +0530

    #include<stdio.h> int main() {     int n,i,arr[100];     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2!=0 && i%2==0)         {             printf("False");             return 0;         }     }     printf("True");     return 0; }

--- Strictly_ODD.c
+++ Strictly_ODD.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int n,arr[100],i,c;
+    int n,i,arr[100];
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
@@ -11,14 +11,10 @@ int main()
     {
         if(arr[i]%2!=0 && i%2==0)
         {
-            c=1;
             printf("False");
-            break;
+            return 0;
         }
     }
-    if(c==0)
-    {
-        printf("True");
-    }
+    printf("True");
     return 0;
 }

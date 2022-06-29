3b719192b7654ae9465529032b7e6b732a12e04c
Vijayalakshmiboya <98221516+Vijayalakshmiboya@users.noreply.github.com>
2022-06-29 23:21:53 +0530

    #include<stdio.h> int main() {     int n,b,i,s=0,c=0,m;     scanf("%d",&n);     int arr[100];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         s=s+arr[i];     }     b=s/n;     for(i=0;i<n;i++)     {         if(b==arr[i])         {             c++;         }     }     if(c>0)     {         printf("True");     }     else     {         printf("False");     } }

--- average_element_is_in_array_or_not.c
+++ average_element_is_in_array_or_not.c
@@ -1,21 +1,21 @@
 #include<stdio.h>
 int main()
 {
-    int n,b,s=0,c=0,m,i;
+    int n,b,i,s=0,c=0,m;
     scanf("%d",&n);
-    int a[100];
+    int arr[100];
     for(i=0;i<n;i++)
     {
-        scanf("%d",&a[i]);
+        scanf("%d",&arr[i]);
     }
     for(i=0;i<n;i++)
     {
-        s=s+a[i];
+        s=s+arr[i];
     }
     b=s/n;
     for(i=0;i<n;i++)
     {
-        if(b==a[i])
+        if(b==arr[i])
         {
             c++;
         }

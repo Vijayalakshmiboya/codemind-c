e615f0d731ddfcf8eaa21b688fb07136ac226084
Vijayalakshmiboya <98221516+Vijayalakshmiboya@users.noreply.github.com>
2022-06-30 23:04:35 +0530

    #include<stdio.h> int main() {     int n,i,arr[100][100],m,j,s1=0,s2=0;     scanf("%d%d",&n,&m);     for(i=0;i<n;i++)     {         for(j=0;j<m;j++)         {             scanf("%d",&arr[i][j]);         }     }     for(i=0;i<n;i++)     {         for(j=0;j<m;j++)         {             if(arr[i][j]%2==0)             {                 s1=s1+arr[i][j];             }         }     }     for(i=0;i<n;i++)     {         for(j=0;j<m;j++)         {             if(arr[i][j]%2==1)             {                 s2=s2+arr[i][j];             }         }     }     printf("%d %d",s1,s2);
    }

---
+++ Sum_of_even_and__odd_elements_in_a_matrix.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,arr[100][100],m,j,s1=0,s2=0;
+    scanf("%d%d",&n,&m);
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<m;j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<m;j++)
+        {
+            if(arr[i][j]%2==0)
+            {
+                s1=s1+arr[i][j];
+            }
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<m;j++)
+        {
+            if(arr[i][j]%2==1)
+            {
+                s2=s2+arr[i][j];
+            }
+        }
+    }
+    printf("%d %d",s1,s2);
+    
+}

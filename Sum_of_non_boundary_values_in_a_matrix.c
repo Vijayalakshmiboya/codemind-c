f7bc8c22dfc9d40d1dfa1d9a77573ed48bcdd5b5
Vijayalakshmiboya <98221516+Vijayalakshmiboya@users.noreply.github.com>
2022-06-30 23:21:27 +0530

    #include<stdio.h> int main() {     int n,i,arr[100][100],m,j,sum=0;     scanf("%d%d",&m,&n);     for(i=0;i<n;i++)     {         for(j=0;j<m;j++)         {             scanf("%d",&arr[i][j]);         }     }     for(i=0;i<n;i++)     {         for(j=0;j<m;j++)         {             if(i!=0 && i!=n-1 && j!=0 &&j!=m-1)             {                 sum=sum+arr[i][j];             }         }     }     printf("%d",sum); }

--- Sum_of_non_boundary_values_in_a_matrix.c
+++ Sum_of_non_boundary_values_in_a_matrix.c
@@ -1,22 +1,22 @@
 #include<stdio.h>
 int main()
 {
-    int i,n,a[100][100],j,sum=0,m;
-    scanf("%d%d",&n,&m);
+    int n,i,arr[100][100],m,j,sum=0;
+    scanf("%d%d",&m,&n);
     for(i=0;i<n;i++)
     {
         for(j=0;j<m;j++)
         {
-            scanf("%d",&a[i][j]);
+            scanf("%d",&arr[i][j]);
         }
     }
     for(i=0;i<n;i++)
     {
         for(j=0;j<m;j++)
         {
-            if(i!=0 && j!=0 && i!=n-1 && j!=m-1 )
+            if(i!=0 && i!=n-1 && j!=0 &&j!=m-1)
             {
-                sum=sum+a[i][j];
+                sum=sum+arr[i][j];
             }
         }
     }
